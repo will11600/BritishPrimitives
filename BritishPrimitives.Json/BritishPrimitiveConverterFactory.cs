dc8c929d13b894f8e3bc475dced2308fd9fa49ac
will11600 <wb11600@gmail.com>
2025-09-29 23:31:01 +0100

    feat: Add BritishPrimitives.Json for System.Text.Json support

---
+++ BritishPrimitives.Json/BritishPrimitiveConverterFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BritishPrimitives.Json;
+
+public sealed class BritishPrimitiveConverterFactory<TConverter, TPrimitive> : JsonConverterFactory
+    where TConverter : JsonConverter<TPrimitive>
+    where TPrimitive : struct, IPrimitive<TPrimitive>
+{
+    public override bool CanConvert(Type typeToConvert)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+}
