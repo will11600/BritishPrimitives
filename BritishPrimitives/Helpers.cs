689eb1d6032a86d91b6d7734dfee6122d93c34ca
will11600 <wb11600@gmail.com>
2025-10-06 17:17:17 +0100

    Refactor: Consolidate fixed-size buffer utilities into `Helpers.cs`

--- BritishPrimitives/Helpers.cs
+++ BritishPrimitives/Helpers.cs
@@ -1,5 +1,4 @@
 ﻿using BritishPrimitives.BitPacking;
-using System.Buffers.Text;
 using System.Runtime.CompilerServices;
 
 namespace BritishPrimitives;
@@ -37,12 +36,6 @@ internal static class Helpers
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ulong Max(uint digitCount, uint baseN = 10U)
-    {
-        return Pow(digitCount, baseN) - 1L;
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool CanWrite(this ref readonly BitWriter writer, int position, int bitLength)
     {
         return position >= 0 && (position + bitLength) < writer.BitLength;
@@ -52,5 +45,51 @@ internal static class Helpers
     public static bool CanRead(this ref readonly BitReader writer, int position, int bitLength)
     {
         return position >= 0 && (position + bitLength) < writer.BitLength;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static unsafe bool SequenceEquals(byte* a, byte* b, int length)
+    {
+        ReadOnlySpan<byte> aSpan = new(a, length);
+        ReadOnlySpan<byte> bSpan = new(b, length);
+        return aSpan.SequenceEqual(bSpan);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static unsafe ulong ConcatenateBytes(byte* ptr, int length)
+    {
+        ulong result = default;
+        for (int i = 0; i < length; i++)
+        {
+            result |= (ulong)ptr[i] << (BitsPerByte * i);
+        }
+        return result;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static unsafe void SpreadBytes(ulong number, byte* ptr, int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            ptr[i] = (byte)(number >> (BitsPerByte * i));
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static unsafe int BuildHashCode(byte* ptr, int length)
+    {
+        HashCode hashCode = new();
+        for (int i = 0; i < length; i++)
+        {
+            hashCode.Add(ptr[i]);
+        }
+        return hashCode.ToHashCode();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int CalculateOffset<T>(ReadOnlySpan<T> source, Range range)
+    {
+        var (offset, length) = range.GetOffsetAndLength(source.Length);
+        return offset + length;
     }
 }
