baad6a11de360220fa74a83d4105c951431b7301
will11600 <wb11600@gmail.com>
2025-10-06 17:21:19 +0100

    Refactor: extracted format specifier interpretation to new PrimitiveFormat utility class

---
+++ BritishPrimitives/PrimitiveFormat.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace BritishPrimitives;
+
+internal static class PrimitiveFormat
+{
+    public const string Specifiers = "GS";
+
+    public static char General => Specifiers[0];
+
+    public static char Spaced => Specifiers[1];
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool TryParse(ReadOnlySpan<char> format, out char specifier)
+    {
+        specifier = format.IsEmpty ? General : char.ToUpperInvariant(format[0]);
+        return format.Length <= 1 && Specifiers.Contains(specifier);
+    }
+}
