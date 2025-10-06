b5999cf701d32138c809525a82c49db55c421188
will11600 <wb11600@gmail.com>
2025-10-06 13:14:15 +0100

    Refactor(BitPacking): Use fixed 6-bit encoding for alphanumeric characters.

--- BritishPrimitives/BitPacking/AlphanumericBitPacker.cs
+++ BritishPrimitives/BitPacking/AlphanumericBitPacker.cs
@@ -1,31 +1,25 @@
-﻿using System.Numerics;
-using System.Runtime.CompilerServices;
-
-namespace BritishPrimitives.BitPacking;
+﻿namespace BritishPrimitives.BitPacking;
 
 internal static class AlphanumericBitPacker
 {
+    public const int SizeInBits = 6;
+
     private const string AlphanumericChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private const uint Base36 = 36;
-    private const uint AlphabetOffset = 10;
-    private const int MaxLength = 12;
+    private const int AlphabetOffset = 10;
 
     public static int UnpackLettersAndNumbers(this ref readonly BitReader reader, ref int position, Span<char> chars)
     {
         int count = 0;
 
+        if (!reader.CanRead(position, chars.Length * SizeInBits))
+        {
+            return count;
+        }
+
         while (count < chars.Length)
         {
-            int length = Math.Min(MaxLength, chars.Length - count);
-            if (!reader.TryUnpackUInt64(ref position, out ulong value, Helpers.Max((uint)length, Base36)))
-            {
-                break;
-            }
-
-            for (var (q, r) = (value, 0UL); q > 0UL; (q, r) = Math.DivRem(q, Base36))
-            {
-                chars[^(count++)] = AlphanumericChars[(int)r];
-            }
+            chars[count++] = AlphanumericChars[reader.ReadByte(position, SizeInBits)];
+            position += SizeInBits;
         }
 
         return count;
@@ -35,38 +29,34 @@ internal static class AlphanumericBitPacker
     {
         int count = 0;
 
-        while (count < chars.Length)
+        if (!writer.CanWrite(position, chars.Length * SizeInBits))
         {
-            ulong encoded = default;
+            return count;
+        }
 
-            int length = Math.Min(MaxLength, chars.Length - count);
-            for (int i = count; i < length; i++)
+        for (int i = count; i < chars.Length; i++)
+        {
+            int normalizedChar;
+            switch (chars[i])
             {
-                uint normalizedChar;
-                switch (chars[i])
-                {
-                    case char c when Character.IsLowercaseLetter(c):
-                        normalizedChar = AlphabetOffset + (uint)Character.Normalize(c, Character.LowercaseA);
-                        break;
-                    case char c when Character.IsUppercaseLetter(c):
-                        normalizedChar = AlphabetOffset + (uint)Character.Normalize(c, Character.UppercaseA);
-                        break;
-                    case char c when Character.IsDigit(c):
-                        normalizedChar = (uint)Character.Normalize(c, Character.Zero);
-                        break;
-                    default:
-                        return count;
-                }
-
-                encoded = encoded * Base36 + normalizedChar;
-                count++;
+                case char c when Character.IsLowercaseLetter(c):
+                    normalizedChar = AlphabetOffset + Character.Normalize(c, Character.LowercaseA);
+                    break;
+                case char c when Character.IsUppercaseLetter(c):
+                    normalizedChar = AlphabetOffset + Character.Normalize(c, Character.UppercaseA);
+                    break;
+                case char c when Character.IsDigit(c):
+                    normalizedChar = Character.Normalize(c, Character.Zero);
+                    break;
+                default:
+                    return count;
             }
 
-            if (!writer.TryPackUInt64(ref position, encoded, Helpers.Max((uint)length, Base36)))
-            {
-                break;
-            }
-        }   
+            writer.WriteByte(position, (byte)normalizedChar, SizeInBits);
+
+            position += SizeInBits;
+            count++;
+        }
 
         return count;
     }
