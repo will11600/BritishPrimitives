4f7cd88324e9c3a7d12fa1623b473d6584f9cdf6
will11600 <wb11600@gmail.com>
2025-10-09 12:25:19 +0100

    feat: Add alphanumeric character bit packing methods

--- BritishPrimitives/BitPacking/AlphanumericBitPacker.cs
+++ BritishPrimitives/BitPacking/AlphanumericBitPacker.cs
@@ -60,4 +60,80 @@ internal static class AlphanumericBitPacker
 
         return count;
     }
+
+    public static bool TryPackAlphanumericLetter(this ref readonly BitWriter writer, ref int position, char letter)
+    {
+        if (writer.CanWrite(position, SizeInBits) && Character.TryNormalizeLetter(letter, out int normalizedChar))
+        {
+            writer.WriteByte(position, (byte)(normalizedChar + AlphabetOffset), SizeInBits);
+            position += SizeInBits;
+            return true;
+        }
+        
+        return false;
+    }
+
+    public static int PackAlphanumericLetters(this ref readonly BitWriter writer, ref int position, ReadOnlySpan<char> chars)
+    {
+        int count = 0;
+
+        if (!writer.CanWrite(position, chars.Length * SizeInBits))
+        {
+            return count;
+        }
+
+        for (; count < chars.Length; count++)
+        {
+            if (Character.TryNormalizeDigit(chars[count], out int normalizedChar))
+            {
+                writer.WriteByte(position, (byte)normalizedChar, SizeInBits);
+                position += SizeInBits;
+                continue;
+            }
+
+            return count;
+        }
+
+        return count;
+    }
+
+    public static bool TryPackAlphanumericDigit(this ref readonly BitWriter writer, ref int position, char letter)
+    {
+        if (writer.CanWrite(position, SizeInBits) && Character.TryNormalizeDigit(letter, out int normalizedChar))
+        {
+            writer.WriteByte(position, (byte)normalizedChar, SizeInBits);
+            position += SizeInBits;
+            return true;
+        }
+        
+        return false;
+    }
+
+    public static bool TryPackAlphanumeric(this ref readonly BitWriter writer, ref int position, char letter)
+    {
+        if (!writer.CanWrite(position, SizeInBits))
+        {
+            return false;
+        }
+
+        int normalizedChar;
+        switch (letter)
+        {
+            case char c when Character.IsUppercaseLetter(c):
+                normalizedChar = Character.Normalize(letter, Character.UppercaseA) + AlphabetOffset;
+                break;
+            case char c when Character.IsLowercaseLetter(c):
+                normalizedChar = Character.Normalize(letter, Character.LowercaseA) + AlphabetOffset;
+                break;
+            case char c when Character.IsDigit(c):
+                normalizedChar = Character.Normalize(letter, Character.Zero);
+                break;
+            default:
+                return false;
+        }
+
+        writer.WriteByte(position, (byte)normalizedChar, SizeInBits);
+        position += SizeInBits;
+        return true;
+    }
 }
