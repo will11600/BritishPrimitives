6d93989d8f21076ec4085793dea85d17d17c347d
will11600 <wb11600@gmail.com>
2025-10-16 00:06:43 +0100

    Refactor `EnumBitPacker` to use `int` for cached enum sizes and simplify calculation.

--- BritishPrimitives/BitPacking/EnumBitPacker.cs
+++ BritishPrimitives/BitPacking/EnumBitPacker.cs
@@ -5,18 +5,18 @@ namespace BritishPrimitives.BitPacking;
 
 internal static class EnumBitPacker
 {
-    private static readonly ConcurrentDictionary<Type, ulong> _enumSizes = [];
+    private static readonly ConcurrentDictionary<Type, int> _enumSizes = [];
 
     public static bool TryPackEnum<T>(this ref readonly BitWriter writer, ref int position, T value) where T : struct, Enum
     {
-        ulong max = _enumSizes.GetOrAdd(typeof(T), CalculateEnumSize);
-        return writer.TryPackInteger(ref position, Convert.ToUInt64(value), max);
+        int max = _enumSizes.GetOrAdd(typeof(T), CalculateEnumSize<T>);
+        return writer.TryPackInteger(ref position, Convert.ToUInt64(value), (ulong)max);
     }
 
     public static bool TryUnpackEnum<T>(this ref readonly BitReader reader, ref int position, out T result) where T : struct, Enum
     {
-        ulong max = _enumSizes.GetOrAdd(typeof(T), CalculateEnumSize);
-        if (reader.TryUnpackInteger(ref position, out ulong resultValue, max))
+        int max = _enumSizes.GetOrAdd(typeof(T), CalculateEnumSize<T>);
+        if (reader.TryUnpackInteger(ref position, out ulong resultValue, (ulong)max))
         {
             result = (T)Enum.ToObject(typeof(T), resultValue);
             return true;
@@ -25,18 +25,19 @@ internal static class EnumBitPacker
         return Helpers.FalseOutDefault(out result);
     }
 
-    private static ulong CalculateEnumSize(Type type)
+    private static int CalculateEnumSize<T>(Type type) where T : struct, Enum
     {
+        var values = Enum.GetValues<T>();
+
         if (type.GetCustomAttribute<FlagsAttribute>() is null)
         {
-            var values = Enum.GetValuesAsUnderlyingType(type).Cast<ulong>();
-            return values.Any() ? values.Max() : 0;
+            return values.Length > 0 ? Convert.ToInt32(values.Max()) : 0;
         }
 
-        ulong max = default;
-        foreach (var value in Enum.GetValuesAsUnderlyingType(type))
+        int max = default;
+        foreach (var value in values)
         {
-            max |= (ulong)value;
+            max |= Convert.ToInt32(value);
         }
 
         return max;
