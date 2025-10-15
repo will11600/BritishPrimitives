4626a10299194743f21b4ca1f6ac108b2e4ad498
will11600 <wb11600@gmail.com>
2025-10-16 00:45:17 +0100

    Optimize EnumBitPacker for `unmanaged` enums and `ulong` values.

--- BritishPrimitives/BitPacking/EnumBitPacker.cs
+++ BritishPrimitives/BitPacking/EnumBitPacker.cs
@@ -1,43 +1,44 @@
 ﻿using System.Collections.Concurrent;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace BritishPrimitives.BitPacking;
 
 internal static class EnumBitPacker
 {
-    private static readonly ConcurrentDictionary<Type, int> _enumSizes = [];
+    private static readonly ConcurrentDictionary<Type, ulong> _enumSizes = [];
 
-    public static bool TryPackEnum<T>(this ref readonly BitWriter writer, ref int position, T value) where T : struct, Enum
+    public static bool TryPackEnum<T>(this ref readonly BitWriter writer, ref int position, T value) where T : unmanaged, Enum
     {
-        int max = _enumSizes.GetOrAdd(typeof(T), CalculateEnumSize<T>);
-        return writer.TryPackInteger(ref position, Convert.ToUInt64(value), (ulong)max);
+        ulong max = _enumSizes.GetOrAdd(typeof(T), CalculateEnumSize<T>);
+        return writer.TryPackInteger(ref position, Convert.ToUInt64(value), max);
     }
 
-    public static bool TryUnpackEnum<T>(this ref readonly BitReader reader, ref int position, out T result) where T : struct, Enum
+    public static unsafe bool TryUnpackEnum<T>(this ref readonly BitReader reader, ref int position, out T result) where T : unmanaged, Enum
     {
-        int max = _enumSizes.GetOrAdd(typeof(T), CalculateEnumSize<T>);
-        if (reader.TryUnpackInteger(ref position, out ulong resultValue, (ulong)max))
+        ulong max = _enumSizes.GetOrAdd(typeof(T), CalculateEnumSize<T>);
+        if (reader.TryUnpackInteger(ref position, out ulong resultValue, max))
         {
-            result = (T)Enum.ToObject(typeof(T), resultValue);
+            result = Unsafe.As<ulong, T>(ref resultValue);
             return true;
         }
 
         return Helpers.FalseOutDefault(out result);
     }
 
-    private static int CalculateEnumSize<T>(Type type) where T : struct, Enum
+    private static ulong CalculateEnumSize<T>(Type type) where T : unmanaged, Enum
     {
         var values = Enum.GetValues<T>();
 
         if (type.GetCustomAttribute<FlagsAttribute>() is null)
         {
-            return values.Length > 0 ? Convert.ToInt32(values.Max()) + 1 : 0;
+            return values.Length > 0 ? Convert.ToUInt64(values.Max()) + 1UL : 0UL;
         }
 
-        int max = default;
+        ulong max = default;
         foreach (var value in values)
         {
-            max |= Convert.ToInt32(value);
+            max |= Convert.ToUInt64(value);
         }
 
         return max;
