daa12099533282793ff4a2f5ad99f7df4eac004a
will11600 <wb11600@gmail.com>
2025-10-16 14:23:10 +0100

    Refactor: Centralize enum value conversions with generic helper

--- BritishPrimitives/BitPacking/EnumBitPacker.cs
+++ BritishPrimitives/BitPacking/EnumBitPacker.cs
@@ -11,15 +11,15 @@ internal static class EnumBitPacker
     public static bool TryPackEnum<T>(this ref readonly BitWriter writer, ref int position, T value) where T : unmanaged, Enum
     {
         ulong max = _enumSizes.GetOrAdd(typeof(T), CalculateEnumSize<T>);
-        return writer.TryPackInteger(ref position, Convert.ToUInt64(value), max);
+        return writer.TryPackInteger(ref position, Convert<T, ulong>(ref value), max);
     }
 
-    public static unsafe bool TryUnpackEnum<T>(this ref readonly BitReader reader, ref int position, out T result) where T : unmanaged, Enum
+    public static bool TryUnpackEnum<T>(this ref readonly BitReader reader, ref int position, out T result) where T : unmanaged, Enum
     {
         ulong max = _enumSizes.GetOrAdd(typeof(T), CalculateEnumSize<T>);
         if (reader.TryUnpackInteger(ref position, out ulong resultValue, max))
         {
-            result = Unsafe.As<ulong, T>(ref resultValue);
+            result = Convert<ulong, T>(ref resultValue);
             return true;
         }
 
@@ -29,18 +29,37 @@ internal static class EnumBitPacker
     private static ulong CalculateEnumSize<T>(Type type) where T : unmanaged, Enum
     {
         var values = Enum.GetValues<T>();
+        return type.GetCustomAttribute<FlagsAttribute>() is null ? Max(values) : AggregateBitwiseOr(values);
+    }
 
-        if (type.GetCustomAttribute<FlagsAttribute>() is null)
-        {
-            return values.Length > 0 ? Convert.ToUInt64(values.Max()) + 1UL : 0UL;
-        }
-
+    private static ulong AggregateBitwiseOr<T>(T[] values) where T : unmanaged, Enum
+    {
         ulong max = default;
-        foreach (var value in values)
+
+        for (int i = 0; i < values.Length; i++)
         {
-            max |= Convert.ToUInt64(value);
+            max |= Convert<T, ulong>(ref values[i]);
         }
 
         return max;
+    }
+
+    private static ulong Max<T>(T[] values) where T : unmanaged, Enum
+    {
+        T max = values.Max();
+        return values.Length > 0 ? Convert<T, ulong>(ref max) + 1UL : 0UL;
+    }
+
+    private static unsafe TResult Convert<TValue, TResult>(ref TValue value) where TValue : unmanaged where TResult : unmanaged
+    {
+        TResult result = default;
+
+        byte* sourcePtr = (byte*)Unsafe.AsPointer(ref value);
+        byte* destinationPtr = (byte*)&result;
+
+        uint byteCount = (uint)Math.Min(sizeof(TValue), sizeof(TResult));
+        Unsafe.CopyBlock(destinationPtr, sourcePtr, byteCount);
+
+        return result;
     }
 }
