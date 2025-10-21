6d74af9828b0a84957c9a699ab580a7ebf67ea2f
will11600 <wb11600@gmail.com>
2025-10-21 04:01:42 +0100

    Refactor: Introduce `IMaxFormattable` and update `IPrimitive` inheritance.

---
+++ BritishPrimitives/IMaxFormattable.cs
@@ -0,0 +1,11 @@
+﻿namespace BritishPrimitives;
+
+/// <summary>
+/// Defines a contract for objects that can be formatted into a span of characters,
+/// have a string representation, and have a maximum length constraint for that string
+/// representation.
+/// </summary>
+public interface IMaxFormattable : ISpanFormattable, IMax
+{
+    // Members are inherited from ISpanFormattable and IMax.
+}
--- BritishPrimitives/IPrimitive.cs
+++ BritishPrimitives/IPrimitive.cs
@@ -9,7 +9,7 @@
 /// This interface enforces static abstract members which are checked by the compiler.
 /// It combines several foundational interfaces to ensure a consistent primitive representation.
 /// </remarks>
-public interface IPrimitive<TSelf> : IEquatable<TSelf>, ISpanParsable<TSelf>, ISpanFormattable, IMax where TSelf : struct, IPrimitive<TSelf>
+public interface IPrimitive<TSelf> : IEquatable<TSelf>, ISpanParsable<TSelf>, IMaxFormattable where TSelf : struct, IPrimitive<TSelf>
 {
     /// <summary>
     /// Determines whether two specified <typeparamref name="TSelf"/> objects have the same value.
